a2f54a3bc550e5889d1b78384dedef764b7c7f8c
Aeon <kheireddine.ounnoughi@etu.sorbonne-universite.fr>
2025-03-18 14:12:45 +0100

    partition algorithm in prototype 9

--- prototype_probleme9.py
+++ prototype_probleme9.py
@@ -73,4 +73,88 @@ def gen_helper(n, k, rank, startPoint):
 
 block , part = gen_helper(5,3,20,1)
 
-print("block : ", block , " part = ", part)
+#print("block : ", block , " part = ", part)
+
+
+@functools.lru_cache(maxsize=None)
+def stirling(n,k):
+    if n == k:
+        return 1
+    if n == 0 or k == 0 : return 0
+
+    return k*RecPart(n-1, k) + RecPart(n-1, k-1)
+
+def R_prefix(n,k,l,d0,d1):
+    left_sum = 0
+    right_sum = 0
+    max_val = n-k-l+1
+    for u in range(1, min(max_val, n-d0) +1 ):
+        left_sum += stirling(n-l-u,k-1) * RecComb(n-d0,u)
+    for u in range(1, min(max_val, n-1-d1) +1):
+        right_sum += stirling(n-l-u,k-1) * RecComb(n-1-d1,u)
+
+    return left_sum - right_sum
+
+
+def extract(n, res ) :
+    l = [i for i in range(1,n+1)]
+    p = []
+    for r in res:
+        q = []
+        for i in r:
+            q.append(l.pop(i))
+            #
+        p.append(q)
+    return p
+
+def next_block(n,k,r):
+    block = [0]
+    acc = stirling(n-1,k-1)
+    if r < acc:
+        return (block,0)
+    d0 = 1
+    index = 2
+    inf = 2
+    sup = n
+    complete = False
+    while not complete:
+        while inf < sup:
+            mid = (inf+sup)//2
+            if r >= acc + R_prefix(n,k,index-1,d0,mid-1):
+                inf = mid+1
+            else:
+                sup = mid
+        mid = inf
+        threshold = stirling(n-index , k -1)
+        acc = acc + R_prefix(n,k,index-1,d0,mid-2)
+        block.append(mid-index)
+        if r < threshold + acc:
+            complete = True
+        else:
+            index +=1
+            d0 = mid
+            inf =  d0+1
+            sup = n
+            acc = acc + threshold
+
+    return (block,acc)
+
+def unranking_lexico(n,k,r):
+    n2 = n
+    res = []
+    while k > 1 :
+        (b,acc) = next_block(n,k,r)
+        res.append(b)
+        r -= acc
+        n -= len(b)
+        k -= 1
+
+    res.append([0 for i in range(n)])
+    res = extract(n2,res)
+    return res
+
+n = 5
+k = 3
+print(stirling(n,k))
+for i in range(0, stirling(n,k)):
+    print("rank = " , i ,"partition = ",unranking_lexico(n,k,i))
